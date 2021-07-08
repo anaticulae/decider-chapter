e78f4c91e43612b49b6d25572fe43759e47633cf
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-08 15:46:19 +0200

    chore(resources): use modern resource generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@
 import genex
 import power
 import pytest
-import utila
 
 import chapter
 
@@ -30,21 +29,14 @@ WORKER = 6
 
 @pytest.mark.usefixtures('session')
 def pytest_sessionstart():
-    power.run([power.generated()])
+    power.run()
 
 
 def extract(resources):
-    # ensure to handle single file generation or common resource subfolder
-    # correctly. To determine the output path it is required to determine
-    # the parent path of at least two files. If resources provide only a
-    # single file the parental determination is not possible. Therefore we
-    # have to add the data root of all test files.
-    utila.log(f'root: {power.REPOSITORY}')
-    resources.append(power.REPOSITORY)
-
     genex.extract(
         files=resources,
         destination=power.generated(),
+        base=power.REPOSITORY,
         groupme=True,
         sections=True,
         words=True,
