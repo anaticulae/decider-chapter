86030d35deb76c0f180875fcae67644a0d56f070
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-05-11 00:35:38 +0200

    test(adjust): adjust unit test

--- tests/chapter/intro/test_decide.py
+++ tests/chapter/intro/test_decide.py
@@ -15,8 +15,8 @@ import chapter.utils
 def test_intro_decide(master72):
     sentences, firstchapter = chapter.utils.content(*master72)
     decided = chapter.intro.decide.run(sentences)
-    assert len(decided) == 39
+    assert len(decided) == 44
     result = chapter.intro.decide.judge(decided, firstchapter)
     assert result.pagestart == 3
     assert result.pageend == 6
-    assert len(result.structure) == 13  # NOT VALIDATED
+    assert len(result.structure) == 14  # NOT VALIDATED
--- tests/chapter/intro/test_section.py
+++ tests/chapter/intro/test_section.py
@@ -13,4 +13,4 @@ import chapter.utils
 def test_intro_content(master72):
     text, sections = master72
     sentences, _ = chapter.utils.content(text, sections)
-    assert len(sentences) == 39  # TODO: NOT VALIDATED YET
+    assert len(sentences) == 44  # TODO: NOT VALIDATED YET
